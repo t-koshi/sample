ce86147a3d714ec10c95b4f30f085fcb29f3fd72
t-koshi <greapflutea2@yahoo.co.jp>
2019-11-08 19:54:46 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <NLink to="/">
+      Top Page
+    </NLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <NLink to="/about">
+      About Page
+    </NLink>
+    <br>
+    <NLink to="/myPage">
+      My Page
+    </NLink>
+  </div>
+</template>
+
+<script>
+</script>
